b34c30194822f820d98e0dfe1201fe444a48c958
easaw <easaw@users.noreply.github.com>
2019-06-15 19:29:21 +0100

    Added mobile colour metadata
    Added 'mobile web app status bar' Metadata with colour: #eee (light grey) for iOS, Android & Win. Can be changed to suit theme colour.

--- header.php
+++ header.php
@@ -13,7 +13,15 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+	<!— Chrome, Firefox OS and Opera —>
+<meta name="theme-color" content="#eee">
+<!— iOS Safari —>
+<meta name="apple-mobile-web-app-status-bar-style" content="#eee">
+<!— iOS Safari —>
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<!— Windows Phone —>
+<meta name="msapplication-navbutton-color" content="#eee">
 	<?php wp_head(); ?>
 </head>
 
